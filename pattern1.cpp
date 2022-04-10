2dd8648a9576d6f531a7cf269b59403cb4ab5769
Rani Uttam Kamble <98833984+rani-ukamble@users.noreply.github.com>
2022-04-10 11:14:31 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,27 @@
+/*
+***
+***
+***
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i, j,r, c;
+	cin>>r>>c;
+	
+	for(i=1; i<=r;i++)
+	{	
+	for(j=1; j<=c;j++)
+		{
+		 cout<<"*";
+		}
+		cout<<endl;
+		
+		
+	}
+	
+	return 0;
+}
