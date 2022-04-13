ef010bce9c1004eccb1c03f8b554851a8f420c7c
Rani Uttam Kamble <98833984+rani-ukamble@users.noreply.github.com>
2022-04-13 19:02:16 +0530

    Add files via upload

---
+++ inbuilt.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a=2, b=3;
+	cout<<a<<" "<<b<<endl;
+	
+	swap(a,b);
+	cout<<a<<" "<<b<<endl;
+	cout<<max(a,b)<<endl;
+	cout<<min(a,b);
+	return 0;
+}
+
+
+
