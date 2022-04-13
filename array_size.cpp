e8aa5630bf21306c3ec5abd1a3b1ba5f5759e5ec
Rani Uttam Kamble <98833984+rani-ukamble@users.noreply.github.com>
2022-04-13 18:50:53 +0530

    Add files via upload

---
+++ array_size.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int n=1e2;
+
+int main()
+{	//int n;
+	//n=1e7;  ->segmentation error
+	int a[n];
+	a[n-1]=9;
+	cout<<a[n-1];
+		
+	return 0;
+}
+
+
+
