d1571a0ed854db1d2b9df50321729c1fdb0b451a
Rani Uttam Kamble <98833984+rani-ukamble@users.noreply.github.com>
2022-04-13 18:56:11 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+	int t=a;
+	a=b;
+	b=t;
+}
+
+int main()
+{
+	int a=2, b=3;
+	cout<<a<<" "<<b<<endl;
+	swap(a,b);
+	cout<<a<<" "<<b<<endl;
+	return 0;
+}
+
+
+
