7204d3094f2e8f80bf1028a56d8b4dddf2cdecc1
Rani Uttam Kamble <98833984+rani-ukamble@users.noreply.github.com>
2022-04-20 07:16:53 +0530

    Update area_friend.cpp

--- area_friend.cpp
+++ area_friend.cpp
@@ -1,30 +1,36 @@
+//find area of square and rectangle
+
 #include <bits/stdc++.h>
 using namespace std;
-
 class box
 {
 	int length;
 	int breadth;
-	int area;
+	int side;
 	public:
-	    void set(int l,int b)
+	    void rect(int l,int b)
+		{		int a;
+		      length=l; breadth=b;		}
+	    void sq(int s)
 		{
 		int a;
-		length=l; breadth=b; area=a;
+		side=s;
 		}
-	friend int show(box);
+	friend int show_rect(box);
+	friend int show_sq(box);
 };
-int show(box b)
+int show_rect(box b1)
+{	return (b1.length*b1.breadth);  }
+int show_sq(box b2)
 {
-	return (b.length*b.breadth);
+	return (b2.side*b2.side);
 }
 int main()
 {
-	box b;
-	b.set(7,2);
-    cout<<"Area of box: "<< show(b)<<endl;
+	box b1,b2;
+	b1.rect(7,2);
+	b2.sq(3);
+    cout<<"Area of rectangle : "<< show_rect(b1)<<endl;
+    cout<<"Area of square : "<< show_sq(b2)<<endl;
 	return 0;
 }
-
-
-
