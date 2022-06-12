b711caad19503d38d46546287462e0a5adb0b77b
Rani Uttam Kamble <98833984+rani-ukamble@users.noreply.github.com>
2022-06-12 15:00:10 +0530

    Create ClassTemplate.cpp

---
+++ ClassTemplate.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//class template
+template <class x>
+//x is datatype called as place holder
+    x big(x a, x b)
+    {
+    if(a>b)
+    return a;
+    else 
+    return b;
+    }
+    
+int main()
+{
+    
+    cout << big(3,5)<<"\n";
+    cout << big(3.6,5.8)<<"\n";
+    return 0;
+}
